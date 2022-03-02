eebe8a66ae9f91970ed249a82ea2b69417d26eca
bora <a52447879@gmail.com>
2022-03-03 00:30:55 +0900

    [feat] 라우팅 용 페이지 추가 및 라우터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,16 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Home, CampDetail, CampApply } from 'pages';
+
 const App = () => {
-  return <div>Hello:)</div>;
+  return (
+    <BrowserRouter basename="howwe">
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="camp/:id" element={<CampDetail />}></Route>
+        <Route path="camp/apply" element={<CampApply />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
