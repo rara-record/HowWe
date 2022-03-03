dc637c6e3f9b4a0bd11dc0a4c2932095a42e3af8
bora <a52447879@gmail.com>
2022-03-03 20:10:59 +0900

    [heat] homeBanner UI 추가

---
+++ src/pages/Home/components/HomeBanner/HomeBanner.tsx
@@ -0,0 +1,13 @@
+const HomeBanner = () => {
+  return (
+    <section className="home-banner">
+      <div className="inner">
+        <div className="row">
+          <h1>현직자와 소통하며 배우는 실무 스킬, 현직자 퍼스널 트레이닝</h1>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default HomeBanner;
