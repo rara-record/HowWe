780c698a4dd5cb4159396d50c5adfd6976f0a03c
bora <a52447879@gmail.com>
2022-03-03 18:45:53 +0900

    [feat] Header, Navbar 마크업

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,10 @@
-import { Navbar, Footer } from 'components';
+import { Header, Footer } from 'components';
 import { Visual, Camp, Community, HomeBanner } from './components';
 
 const Home = () => {
   return (
     <>
-      <Navbar />
+      <Header />
       <main>
         <Visual />
         <Camp />
