867349f9574cbeb1b088da3cff6176db1201027d
bora <a52447879@gmail.com>
2022-03-04 17:09:22 +0900

    [feat] 커뮤니티 상세페이지 추가 및 라우터 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './css/style.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Home, CampDetail, CampApply } from 'pages';
+import { Home, CampDetail, CampApply, CommunityDetail } from 'pages';
 
 const App = () => {
   return (
@@ -9,6 +9,7 @@ const App = () => {
         <Route path="/" element={<Home />}></Route>
         <Route path="camp/:id" element={<CampDetail />}></Route>
         <Route path="camp/apply" element={<CampApply />}></Route>
+        <Route path="community/:id" element={<CommunityDetail />}></Route>
       </Routes>
     </BrowserRouter>
   );
