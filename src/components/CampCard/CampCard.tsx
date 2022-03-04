ce14daf6751c58cf4ac11ef7cb6b489864a6e4a5
bora <a52447879@gmail.com>
2022-03-04 16:29:13 +0900

    [feat] CampCard, CommunityCard 레이아웃 추가

--- src/components/CampCard/CampCard.tsx
+++ src/components/CampCard/CampCard.tsx
@@ -3,15 +3,26 @@ import { Link } from 'react-router-dom';
 const CampCard = () => {
   return (
     <>
-      <Link to="/camp/1">
-        <div>Camp 1</div>
-      </Link>
-      <Link to="/camp/2">
-        <div>Camp 2</div>
-      </Link>
-      <Link to="/camp/3">
-        <div>Camp 3</div>
-      </Link>
+      <article className="camp-card">
+        <Link to="/camp/1">
+          <div>Camp 1</div>
+        </Link>
+      </article>
+      <article className="camp-card">
+        <Link to="/camp/2">
+          <div>Camp 2</div>
+        </Link>
+      </article>
+      <article className="camp-card">
+        <Link to="/camp/3">
+          <div>Camp 3</div>
+        </Link>
+      </article>
+      <article className="camp-card">
+        <Link to="/camp/4">
+          <div>Camp 4</div>
+        </Link>
+      </article>
     </>
   );
 };
