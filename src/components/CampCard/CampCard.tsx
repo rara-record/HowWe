2346038a1a9ee00595f15204b21e51b67e23a3b6
bora <a52447879@gmail.com>
2022-03-11 19:50:13 +0900

    [feat] Card UI 초기 세팅

--- src/components/CampCard/CampCard.tsx
+++ src/components/CampCard/CampCard.tsx
@@ -1,26 +1,16 @@
-import { Link } from 'react-router-dom';
-import { ICamp } from 'types/type';
+import styled from 'styled-components';
 
-const CampCard = ({ camp }: { camp: ICamp }) => {
-  return (
-    <>
-      <article
-        className="camp-card"
-        style={{
-          backgroundImage: `url(${camp.thumbnail})`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          padding: '20px',
-        }}
-      >
-        <Link to={`/camp/${camp.id}`}>
-          <div>{camp.status}</div>
-          <div>{camp.name}</div>
-          <div>{camp.startDate}</div>
-        </Link>
-      </article>
-    </>
-  );
+interface IProps {
+  bgImg: string;
+  title: string;
+  subTitle: string;
+  startDate: string;
+}
+
+const CampCard = ({ bgImg, title, subTitle, startDate }: IProps) => {
+  return <Container></Container>;
 };
 
 export default CampCard;
+
+const Container = styled.div``;
