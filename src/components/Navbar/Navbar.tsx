1e0fbdc7dd4bcf96074e8ee4e84730281e49acea
bora <a52447879@gmail.com>
2022-03-11 18:48:35 +0900

    [feat] Navbar 스크롤시 디자인 바뀌는 기능 업데이트

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,22 +1,49 @@
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { maxWidth } from 'styles/mixin';
 
 const Navbar = () => {
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(() => {
+    changeBackground();
+    window.addEventListener('scroll', changeBackground);
+  });
+
+  const changeBackground = () => {
+    if (window.scrollY >= 80) {
+      setIsScrolled(true);
+    } else {
+      setIsScrolled(false);
+    }
+  };
+
   return (
-    <Container>
+    <Container isScrolled={isScrolled}>
       <div className="main">
-        <Link to="/">
-          <img
-            className="logo"
-            src={require('assets/images/logo-primary.png')}
-            alt="로고"
-          />
-        </Link>
+        <h1 className="logo">
+          <Link to="/">
+            <img
+              className="logo-img"
+              src={
+                isScrolled
+                  ? require('assets/images/logo-primary.png')
+                  : require('assets/images/logo-white.png')
+              }
+              alt="로고"
+            />
+          </Link>
+        </h1>
+
         <Link to="community">
           <img
             className="ic-person"
-            src={require('assets/images/icons/ic-person-primary.png')}
+            src={
+              isScrolled
+                ? require('assets/images/icons/ic-person-primary.png')
+                : require('assets/images/icons/ic-person-white.png')
+            }
             alt="프로필"
           />
         </Link>
@@ -27,10 +54,17 @@ const Navbar = () => {
 
 export default Navbar;
 
-const Container = styled.nav`
+const Container = styled.nav<{ isScrolled: boolean }>`
   position: fixed;
   width: 100%;
   z-index: 100;
+
+  ${props =>
+    props.isScrolled &&
+    css`
+      background-color: white;
+      transition: 0.5s;
+    `}
 
   .main {
     ${maxWidth}
@@ -42,7 +76,17 @@ const Container = styled.nav`
 
   .logo {
     width: 80px;
-    height: 22px;
+    height: 32px;
+  }
+
+  a {
+    display: block;
+  }
+
+  .logo-img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
   }
 
   .ic-person {
