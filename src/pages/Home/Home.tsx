381687531d984982b32c91a36b1fd9b6a4b86b5c
bora <a52447879@gmail.com>
2022-03-11 22:11:37 +0900

    [refactor] Home 레이아웃 변경

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
-import { Header, Footer, CampCard } from 'components';
+import { Navbar, Footer } from 'components';
+import { HeaderSection, CampSection } from './components';
 import { ICamp } from 'types/type';
+import styled from 'styled-components';
 
 // TODO: 실서버 데이터로 변경
-const camp: ICamp = {
+const campMock: ICamp = {
   id: 0,
   name: '업무 단순화 & 자동화로 엑셀을 실무에 더 적극 활용하기',
   type: '인기',
@@ -13,29 +15,24 @@ const camp: ICamp = {
   thumbnail: 'https://cdn.comento.kr/images/pt/tmp/prefix_44UsYDVNuM.jpg',
 };
 
-// TODO: props 전달하면서, campCard에 props정의를 해주지 않았더니 IntrinsicAttributes 에러가 났다.
 const Home = () => {
   return (
-    <>
-      <Header />
+    <Container>
+      <Navbar />
+      <HeaderSection />
       <main>
-        <section>
-          <div>인기 부트 캠프</div>
-          <div>
-            <CampCard camp={camp} />
-          </div>
-        </section>
-
-        <section>
-          <div>특가 할인 캠프</div>
-          <div>
-            <CampCard camp={camp} />
-          </div>
-        </section>
+        <CampSection title="인기 부트 캠프" camps={campMock} />
+        <CampSection title="특가 할인 캠프" camps={campMock} isHeadField />
       </main>
       <Footer />
-    </>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  main {
+    height: 200vh;
+  }
+`;
 
 export default Home;
