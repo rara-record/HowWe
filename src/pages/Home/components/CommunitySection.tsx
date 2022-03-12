55aea89b23e5274253d24b879429372d7f1da4b3
bora <a52447879@gmail.com>
2022-03-12 14:51:29 +0900

    [feat] Community mock데이터 돌려서 CommunityCard에 전달

--- src/pages/Home/components/CommunitySection.tsx
+++ src/pages/Home/components/CommunitySection.tsx
@@ -2,11 +2,12 @@ import { CommunityCard } from 'components';
 import { ICommunity } from 'types/type';
 
 import styled from 'styled-components';
+import fonts from 'styles/fonts';
 import { maxWidth } from 'styles/mixin';
 
 interface Props {
   title: string;
-  communities: ICommunity;
+  communities: ICommunity[];
 }
 
 const CommunitySection = ({ title, communities }: Props) => {
@@ -14,7 +15,9 @@ const CommunitySection = ({ title, communities }: Props) => {
     <Container>
       <div className="section-title">{title}</div>
       <div className="cards">
-        <CommunityCard community={communities} />
+        {communities.map((community, index) => (
+          <CommunityCard key={index} community={community} />
+        ))}
       </div>
     </Container>
   );
@@ -24,4 +27,14 @@ export default CommunitySection;
 
 const Container = styled.section`
   ${maxWidth}
+  padding: 0px 16px;
+
+  .section-title {
+    ${fonts.H1};
+    padding-bottom: 16px;
+  }
+  .cards {
+    display: flex;
+    gap: 20px;
+  }
 `;
