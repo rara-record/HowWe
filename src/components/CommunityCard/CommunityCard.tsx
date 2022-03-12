4af032b8c475c34b13a7fddb1246dbce99f03be5
bora <a52447879@gmail.com>
2022-03-12 14:52:00 +0900

    [feat] CommunityCard UI 업데이트

--- src/components/CommunityCard/CommunityCard.tsx
+++ src/components/CommunityCard/CommunityCard.tsx
@@ -1,30 +1,59 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
-const CommunityCard = () => {
+import { ICommunity } from 'types/type';
+import fonts from 'styles/fonts';
+import { Tags, Comments } from 'components';
+
+interface IProps {
+  community: ICommunity;
+}
+
+const CommunityCard = ({ community }: IProps) => {
   return (
-    <>
-      <article className="community-card">
-        <Link to="/community/1">
-          <div>community 1</div>
-        </Link>
-      </article>
-      <article className="community-card">
-        <Link to="/community/2">
-          <div>community 2</div>
-        </Link>
-      </article>
-      <article className="community-card">
-        <Link to="/community/3">
-          <div>community 3</div>
-        </Link>
-      </article>
-      <article className="community-card">
-        <Link to="/community/4">
-          <div>community 4</div>
-        </Link>
-      </article>
-    </>
+    <Link to={`/community/${community.id}`}>
+      <Container>
+        <div>
+          <Tags tags={community.tags} />
+          <div className="title">{community.title}</div>
+          <div className="content">{community.content}</div>
+        </div>
+        <Comments comments={community.comments} />
+        <button type="button">+ 더보기</button>
+      </Container>
+    </Link>
   );
 };
 
 export default CommunityCard;
+
+const Container = styled.article`
+  padding: 16px;
+  height: 320px;
+
+  border: 1px solid #cccccc;
+  box-shadow: 0px 8px 8px rgba(0, 0, 0, 0.1);
+  border-radius: 10px;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .title {
+    padding-top: 8px;
+    ${fonts.Body1};
+    font-weight: bold;
+  }
+  .content {
+    padding-top: 10px;
+    ${fonts.Body1};
+  }
+
+  img {
+    background-size: cover;
+    background-position: center;
+    width: 30px;
+    height: 30px;
+    border-radius: 15px;
+  }
+`;
