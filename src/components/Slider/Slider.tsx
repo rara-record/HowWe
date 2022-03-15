3401df7e0d5b3bdecdfa584e9d9ad46462238246
bora <a52447879@gmail.com>
2022-03-15 22:54:49 +0900

    [feat] slider 컴포넌트 추가

---
+++ src/components/Slider/Slider.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+import { ISlider } from 'types/type';
+
+interface Props {
+  slider: ISlider[];
+  isMobile: boolean;
+}
+
+const Slider = ({ slider, isMobile }: Props) => {
+  return <Container></Container>;
+};
+
+export default Slider;
+
+const Container = styled.div``;
