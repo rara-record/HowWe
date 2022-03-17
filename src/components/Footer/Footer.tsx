d7542d4be0bd72a951d26ec3228ddc9e3b90b045
bora <a52447879@gmail.com>
2022-03-17 16:43:04 +0900

    [design] Footer 반응형  디자인 수정

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,64 +1,99 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 import { maxWidth } from 'styles/mixin';
 import colors from 'styles/colors';
 import fonts from 'styles/fonts';
+import { useMediaQuery } from 'react-responsive';
 
 const Footer = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
   return (
-    <Container>
+    <Container isMobile={isMobile}>
       <footer>
-        <div>
+        <div className="footer-logo">
           <img
-            className="logo"
+            className="logo-img"
             src={require('assets/images/logo-primary.png')}
             alt="로고"
           />
         </div>
-        <div className="content">
-          <div>
-            대표: 내이름. 서울특별시 중구 한강대로 416, 서울스퀘어 15층 101호
+        <div className="footer-info">
+          <div className="content">
+            <p>
+              대표: 내이름. 서울특별시 중구 한강대로 416, 서울스퀘어 15층 101호
+            </p>
+            <br />
+            <p> Copyright by (주)카페인.</p>
+            <p> All right reserved. 이용약관 개인정보처리방침</p>
           </div>
-          <br />
-          <div> Copyright by (주)카페인.</div>
-          <div> All right reserved. 이용약관 개인정보처리방침</div>
-        </div>
-        <div className="sns">
-          <div className="sns-item"></div>
-          <div className="sns-item"></div>
-          <div className="sns-item"></div>
+          <ul className="sns">
+            <li className="sns-item"></li>
+            <li className="sns-item"></li>
+            <li className="sns-item"></li>
+          </ul>
         </div>
       </footer>
     </Container>
   );
 };
 
-const Container = styled.div`
+const Container = styled.div<{ isMobile: boolean }>`
   padding: 32px;
   background-color: ${colors.gray6};
 
   footer {
     ${maxWidth};
     display: flex;
-    .logo {
+
+    .logo-img {
       width: 60px;
       padding-right: 115px;
     }
+
+    .footer-info {
+      display: flex;
+      flex-wrap: wrap;
+      justify-content: space-between;
+      width: 100%;
+
+      ${props =>
+        props.isMobile &&
+        css`
+          display: block;
+        `}
+    }
+
     .content {
       flex: 1; // 남은 공간만큼 꽉 채우기
       ${fonts.Caption}
       color: ${colors.gray2};
+
+      ${props =>
+        props.isMobile &&
+        css`
+          padding-bottom: 15px;
+        `}
     }
+
     .sns {
       display: flex;
+      justify-content: flex-end;
       gap: 6px;
       padding-left: 10px;
-      div {
+
+      li {
         width: 32px;
         height: 32px;
         background-color: ${colors.gray4};
         border-radius: 16px;
       }
+
+      ${props =>
+        props.isMobile &&
+        css`
+          padding-left: 0;
+        `}
     }
   }
 `;
