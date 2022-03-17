281b60dc81cf15286ab1f5995fe840d59ffa4059
bora <a52447879@gmail.com>
2022-03-17 20:51:06 +0900

    [feat] 조건부 Narbar Style 적용

--- src/pages/CampDetail/CampDetail.tsx
+++ src/pages/CampDetail/CampDetail.tsx
@@ -1,17 +1,20 @@
 import { Link } from 'react-router-dom';
 import { Navbar } from 'components';
+import styled from 'styled-components';
 
 const CampDetail = () => {
   return (
-    <>
-      <Navbar />
+    <Container>
+      <Navbar type={'sub'} />
       <div>상세페이지</div>
 
       <button>
         <Link to="/camp/apply">더잘하는개발자되기</Link>
       </button>
-    </>
+    </Container>
   );
 };
 
 export default CampDetail;
+
+const Container = styled.div``;
