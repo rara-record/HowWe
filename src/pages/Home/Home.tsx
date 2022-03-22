a67162f8ed6c5bf5a0ec81f5bbbd41c0a3bca337
bora <a52447879@gmail.com>
2022-03-23 03:30:22 +0900

    [comment] Home fetchCamps 함수 주석 추가

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -41,8 +41,8 @@ const Home = () => {
   });
 
   useEffect(() => {
-    featchCamps('popular');
-    featchCamps('sale');
+    fetchCamps('popular');
+    fetchCamps('sale');
     setCommunities([
       communityMock,
       communityMock,
@@ -51,9 +51,10 @@ const Home = () => {
     ]);
   }, []);
 
-  const featchCamps = async (type: CampType) => {
+  // TODO: 비동기 함수는 바로 useEffect에서 직접적으로 state에 담는 것은 권장되는 사항이 아니기 때문에, 비동기를 처리하는 함수를 만들어서 한번 더 감싸준 후 처리해주는 것이 좋다.
+  const fetchCamps = async (type: CampType) => {
     const camps = await getCampsByType(type);
-    type === 'popular' ? setPopularCamps(camps) : setSaleCamps(camps);
+    type === 'popular' ? setPopularCamps(camps) : setSaleCamps(camps); // TODO: type별로 나누어지지 않음
   };
 
   return (
@@ -63,12 +64,15 @@ const Home = () => {
       <main>
         <CampSection title="인기 부트 캠프" camps={popularCamps} />
         <Padding height="40px" />
+
         <CampSection title="특가 할인 캠프" camps={saleCamps} isHeadField />
         <Padding height="40px" />
+
         <HomeBanner
           text={`현직자와 소통하며 배우는\n실무 스킬, 퍼스널 트레이닝`}
         />
         {isDesktop && <Padding height="55px" />}
+
         <CommunitySection title="커뮤니티" communities={communities} />
         {isDesktop && <Padding height="240px" />}
       </main>
