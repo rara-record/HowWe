c53d4d9b7ff628e1eb1b9cfd525088aa8f0d2ae2
bora <a52447879@gmail.com>
2022-03-30 13:53:31 +0900

    [feat] CampStore 추가

---
+++ src/stores/CampStroe.tsx
@@ -0,0 +1,27 @@
+import { getCampsByType } from 'apis/camp';
+import { makeObservable, observable } from 'mobx';
+import { createContext } from 'react';
+import { ICamp } from 'types/Camp';
+
+class CampsStore {
+  // 변수 선언
+  @observable campPopular: ICamp[] | null = null;
+  @observable campSales: ICamp[] | null = null;
+
+  // 고정
+  constructor() {
+    makeObservable(this);
+  }
+
+  fetchCampsPopular = async () => {
+    const data = await getCampsByType('popular');
+    this.campPopular = data;
+  };
+
+  fetchCampsSale = async () => {
+    const data = await getCampsByType('sale');
+    this.campSales = data;
+  };
+}
+
+export default createContext(new CampsStore());
