6178883c6cc768571ad3d569b12291131e7805b9
bora <a52447879@gmail.com>
2022-04-01 00:28:59 +0900

    [refactor] CampDetail ContentSection  컴포넌트 추가

--- src/pages/CampDetail/index.tsx
+++ src/pages/CampDetail/index.tsx
@@ -5,7 +5,8 @@ import CampsStore from 'stores/CampsStore';
 import styled from 'styled-components';
 import Tags from 'components/Tags';
 import fonts from 'styles/fonts';
-import { Padding, Sidebar, Skeleton } from 'components';
+import { Sidebar, Skeleton } from 'components';
+import { ContentSection } from './components';
 
 const CampDetail = () => {
   const { campId } = useParams();
@@ -21,9 +22,9 @@ const CampDetail = () => {
       <Container className="camp-detail-container">
         <BannerBackground />
 
-        <div className="inner">
-          {/* 캠프 상세페이지 비주얼 */}
-          <VisualSection>
+        {/* 캠프 상세페이지 비주얼 */}
+        <VisualSection>
+          <div className="inner">
             <div className="camp-detail-visual-title">
               <Tags tags={['2기모집']} />
               <h1>{campStore.targetCamp.name}</h1>
@@ -36,56 +37,17 @@ const CampDetail = () => {
                 alt="camp-detail-visual-img"
               />
             </figure>
-          </VisualSection>
+          </div>
+        </VisualSection>
 
-          <main>
-            {/* 컨텐츠 */}
-            <ContentSection>
-              <InfoContainer>
-                <h1>
-                  대답없는 VOD 강의에 <strong>라이브</strong>로 답하다.
-                </h1>
-                <p>
-                  React만큼은 실무에 제대로 활용할 수 있도록, <br></br>오프라인
-                  강의와 온라인 VOD의 장점만 모았습니다.
-                </p>
+        <main>
+          {/* 컨텐츠 */}
+          <ContentSection targetCamp={campStore.targetCamp} />
 
-                <div className="info-box">
-                  <article>
-                    <h2>LIVE CLASS</h2>
-                    <p>라이브로 묻고 해답을 얻으세요.</p>
-                  </article>
-
-                  <hr className="line" />
-
-                  <article>
-                    <h2>KEEP DOING</h2>
-                    <p>미루지 말고 실시간으로 만나요.</p>
-                  </article>
-
-                  <hr className="line" />
-
-                  <article>
-                    <h2>CAN DO</h2>
-                    <p>실무 과제를 풀며 제대로 활용해요.</p>
-                  </article>
-                </div>
-              </InfoContainer>
-
-              <ImageContainer>
-                {campStore.targetCamp.images.map((img, index) => (
-                  <article key={index}>
-                    <img src={img} alt="camp-detail-img" />
-                    <Padding height="40px" />
-                  </article>
-                ))}
-              </ImageContainer>
-            </ContentSection>
-
-            {/* 사이드바 */}
-            <Sidebar targetCamp={campStore.targetCamp} />
-          </main>
-        </div>
+          {/* 사이드바 */}
+          <Sidebar targetCamp={campStore.targetCamp} />
+          {/* 사이드바 */}
+        </main>
       </Container>
     );
   } else {
@@ -104,10 +66,6 @@ export default CampDetail;
 
 const Container = styled.div`
   position: relative;
-
-  .inner {
-    ${maxWidth}
-  }
 
   main {
     display: flex;
@@ -129,11 +87,14 @@ const BannerBackground = styled.div`
 
 const VisualSection = styled.div`
   position: relative;
-  display: flex;
-  justify-content: space-between;
-  height: 340px;
-  padding: 40px 16px 0;
   z-index: 1;
+
+  .inner {
+    display: flex;
+    justify-content: space-between;
+    height: 340px;
+    padding: 40px 16px 0;
+  }
 
   .camp-detail-visual-title {
     flex: 1;
@@ -166,73 +127,4 @@ const VisualSection = styled.div`
       border-radius: 4px;
     }
   }
-`;
-
-const ContentSection = styled.section`
-  flex: 2;
-  padding: 10px;
-`;
-
-const InfoContainer = styled.div`
-  letter-spacing: 0.1px;
-
-  h1 {
-    font-size: 24px;
-    font-weight: 600;
-    line-height: 28px;
-    margin: 20px 0;
-    color: #040505;
-
-    strong {
-      color: #971818;
-    }
-  }
-
-  p {
-    font-size: 17px;
-    font-weight: 400;
-    line-height: 25px;
-    color: #3c4144;
-  }
-
-  .info-box {
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    padding: 18px 24px;
-    margin-top: 20px;
-    background-color: #fcfcfc;
-
-    article {
-      position: relative;
-
-      h2 {
-        display: flex;
-        align-items: center;
-        font-size: 17px;
-        font-weight: 600;
-        line-height: 30px;
-        color: #202325;
-      }
-
-      p {
-        font-size: 15px;
-        line-height: 20px;
-        font-weight: 400;
-        color: #595f63;
-        letter-spacing: 0.2px;
-      }
-    }
-  }
-
-  .line {
-    border-left: 1px;
-    border-color: #eaecee;
-    height: inherit;
-  }
-`;
-
-const ImageContainer = styled.div`
-  width: 100%;
-  height: 100%;
 `;
