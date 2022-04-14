37d9886ff809d42f4af762a5e3832b391bb057ab
bora <a52447879@gmail.com>
2022-04-14 22:49:50 +0900

    [feat] 파이어베이스 연동

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
+import { firestore } from './utils/firebase';
+console.log(firestore);
 ReactDOM.render(
   <React.StrictMode>
     <App />
