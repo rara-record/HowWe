084bace93d6d1e97a2163c3c60cbbfdc9351c8d7
bora <a52447879@gmail.com>
2022-04-19 06:50:44 +0900

    [feat] 파이어베이스 데이터 베이스 연동

---
+++ src/data/community.ts
@@ -0,0 +1,20 @@
+import { db } from '../service/firebase';
+import { ICommunity } from '../types/Community';
+import { collection, query, getDocs } from 'firebase/firestore';
+
+export const getCommunties = async () => {
+  const data: ICommunity[] = [];
+  const q = query(collection(db, 'communities'));
+  const communities = await getDocs(q);
+
+  communities.docs.map(doc =>
+    data.push({
+      id: doc.data().id,
+      tags: doc.data().tags,
+      title: doc.data().title,
+      content: doc.data().content,
+      comments: doc.data().comments,
+    })
+  );
+  return data;
+};
