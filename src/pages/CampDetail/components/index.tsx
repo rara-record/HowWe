e78605076c3a94a5a1139c4d8cf89c8880c056d4
bora <a52447879@gmail.com>
2022-04-20 06:32:23 +0900

    [refactor] 캠프 상세페이지 최종

--- src/pages/CampDetail/components/index.tsx
+++ src/pages/CampDetail/components/index.tsx
@@ -1,6 +1,5 @@
 export { default as HeaderSection } from './HeaderSection';
 export { default as InfoSection } from './InfoSection';
-export { default as ImageSection } from './ImageSection';
 export { default as ReviewSection } from './ReviewSection';
 export { default as FaqSection } from './FaqSection';
 export { default as Sidebar } from './Sidebar';
