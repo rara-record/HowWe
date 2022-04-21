d58e3c24e1046b6eb96f9b7701a3b2a67ffc0b89
bora <a52447879@gmail.com>
2022-04-22 02:43:06 +0900

    [fix] AuthStore 로직 수정

--- src/stores/AuthStore.tsx
+++ src/stores/AuthStore.tsx
@@ -1,8 +1,8 @@
-import { getSignIn, getSignOut } from 'data/auth';
+import { getSignUp, getLogin } from 'api/auth';
 import { makeObservable, observable } from 'mobx';
 import { createContext } from 'react';
 
-class AuthStore2 {
+class AuthStore {
   @observable token: string | null = '';
   @observable isLoggedIn: boolean = false;
 
@@ -10,17 +10,25 @@ class AuthStore2 {
     makeObservable(this);
   }
 
-  loginHandler = async (email: string, password: string) => {
-    const data = await getSignIn(email, password);
+  signUpHandler = async (email: string, password: string) => {
+    const data = await getSignUp(email, password);
     this.token = data;
-    this.isLoggedIn = true;
   };
 
-  logoutHandler = async (email: string, password: string) => {
-    await getSignOut(email, password);
-    this.token = null;
-    this.isLoggedIn = false;
+  loginHandler = async (email: string, password: string) => {
+    const data = await getLogin(email, password);
+
+    if (data) {
+      this.token = data;
+      this.isLoggedIn = true;
+    }
   };
+
+  // logoutHandler = async (email: string, password: string) => {
+  //   await getSignIn(email, password);
+  //   this.token = null;
+  //   this.isLoggedIn = false;
+  // };
 }
 
-export default createContext(new AuthStore2());
+export default createContext(new AuthStore());
