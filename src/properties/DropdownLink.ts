9f931fcf82d14d608c72e41a07115af544e3502f
bora <a52447879@gmail.com>
2022-07-23 02:31:17 +0900

    [feat] properties 폴더 추가

---
+++ src/properties/DropdownLink.ts
@@ -0,0 +1,26 @@
+export const DROPDWON_LINK = [
+  {
+    display: '강의목록',
+    path: '/profile',
+  },
+  {
+    display: 'Q&A',
+    path: '/profile',
+  },
+  {
+    display: '결제 내역 / 환불',
+    path: '/profile',
+  },
+  {
+    display: '내 쿠폰',
+    path: '/profile',
+  },
+  {
+    display: '회원 정보 수정',
+    path: '/profile',
+  },
+  {
+    display: '비밀번호 변경',
+    path: '/profile',
+  },
+];
