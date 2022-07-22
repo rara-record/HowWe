a8d5cb8c9643138803a0c4d1d4fe9bae75a83ed1
bora <a52447879@gmail.com>
2022-07-23 02:31:38 +0900

    [feat] Profile 페이지 추가

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,9 +1,19 @@
 import styled from 'styled-components';
+import fonts from 'styles/fonts';
+
+import { useContext } from 'react';
+import UserStore from 'stores/UserStore';
+
 import ProfileForm from './components/ProfileForm';
 
 const ProfilePage = () => {
+  const userStore = useContext(UserStore);
+
   return (
     <Container>
+      <ProfileHeader>
+        <h1> {userStore.user && userStore.user.displayName}님 안녕하세요!</h1>
+      </ProfileHeader>
       <ProfileForm />
     </Container>
   );
@@ -11,4 +21,11 @@ const ProfilePage = () => {
 
 export default ProfilePage;
 
-const Container = styled.section``;
+const Container = styled.section`
+  h1 {
+    text-align: center;
+    ${fonts.H1}
+  }
+`;
+
+const ProfileHeader = styled.div``;
