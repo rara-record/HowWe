a5a106e360adf90187eb4d8ca03ab8fcb1f3d5d3
bora <a52447879@gmail.com>
2022-07-23 02:25:44 +0900

    [feat] AuthStore 리팩토링

--- src/stores/AuthStore.ts
+++ src/stores/AuthStore.ts
@@ -1,5 +1,4 @@
 import firebase from 'firebase/compat/app';
-
 import { createContext } from 'react';
 import { makeObservable, observable } from 'mobx';
 import { getSignUp, getSignIn } from 'api/userAuth';
@@ -8,37 +7,30 @@ class AuthStore {
   @observable user: firebase.User | null | firebase.auth.UserCredential | void =
     null;
   @observable isLoggedIn: boolean = false;
-  @observable isAuthFail: boolean = false;
 
   constructor() {
     makeObservable(this);
   }
 
   signUp = async (name: string, email: string, password: string) => {
-    const userInfo = await getSignUp(name, email, password);
-    console.log(userInfo);
+    await getSignUp(name, email, password);
   };
 
   signIn = async (email: string, password: string) => {
-    const apiRes = await getSignIn(email, password);
-    console.log(apiRes);
-    apiRes ? (this.isLoggedIn = true) : (this.isLoggedIn = false);
+    const res = await getSignIn(email, password);
+    this.user = res;
+
+    if (this.user) {
+      this.isLoggedIn = true;
+    } else {
+      this.isLoggedIn = false;
+    }
   };
 
   signOut = () => {
     this.user = null;
     this.isLoggedIn = false;
   };
-
-  // @action newPassword = async (
-  //   token: string | null,
-  //   enterdNewPassword: string
-  // ) => {
-  //   if (token) {
-  //     await changeNewPassword(token, enterdNewPassword);
-  //     this.isLoggedIn = false;
-  //   }
-  // };
 }
 
 export default createContext(new AuthStore());
